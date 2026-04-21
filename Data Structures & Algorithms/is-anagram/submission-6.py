f669668693b93d3b642e110f4dd2edf976fa46a8
zfan747 <fanzhen0408@gmail.com>
2026-04-21 21:58:46 +1200

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        for i in range(len(s)-1, -1, -1):
+            if s[i] in t:
+                t = t.replace(s[i], "", 1)
+            else:
+                return False
+        if len(t) == 0:
+            return True
+        else:
+            return False

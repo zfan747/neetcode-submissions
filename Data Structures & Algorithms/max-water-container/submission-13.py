53f161f0ad8055ae80ef757ca31a0a3f90bd5770
zfan747 <fanzhen0408@gmail.com>
2026-04-22 18:01:03 +1200

    Add: max-water-container - submission-13

---
+++ Data Structures & Algorithms/max-water-container/submission-13.py
@@ -0,0 +1,39 @@
+class Solution:
+    def maxArea(self, heights: List[int]) -> int:
+        largest = 0
+        for i in range(len(heights)):
+            for j in range(i+1, len(heights)):
+                if (min(heights[i], heights[j]) * (j-i)) > largest:
+                    largest = min(heights[i], heights[j]) * (j-i)
+                else:
+                    pass
+        return largest
+
+
+        # largest = 0
+        # max_height = max(heights)
+        # # print(max_height)
+        # if heights.count(max_height) > 1:
+        #     normal_pos = heights.index(max_height)
+        #     reverse_pos = heights[::-1].index(max_height)
+        #     reverse_pos = len(heights) - reverse_pos - 1
+        #     # print(normal_pos, reverse_pos)
+        #     best_pos = abs(max(abs(normal_pos - len(heights)), abs(reverse_pos - len(heights))) - len(heights))
+        # else:
+        #     best_pos = heights.index(max_height)
+        # # print(best_pos)
+        # # print(heights[best_pos])
+        # for i in range(best_pos):
+        #     for j in range(i+1, len(heights)):
+        #         if (min(heights[i], heights[j]) * (j-i)) > largest:
+        #             largest = min(heights[i], heights[j]) * (j-i)
+        # # print(largest)
+        # for j in range(best_pos + 1, len(heights)):
+        #     if (min(heights[best_pos], heights[j]) * abs(j-best_pos)) > largest:
+        #         largest = min(heights[best_pos], heights[j]) * abs(j-best_pos)
+        # return largest
+
+
+
+
+

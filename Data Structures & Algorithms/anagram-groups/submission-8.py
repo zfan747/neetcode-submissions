5712377600e4d716cc6df6e67b2e6751417249e4
zfan747 <fanzhen0408@gmail.com>
2026-04-23 16:52:16 +1200

    Add: anagram-groups - submission-8

---
+++ Data Structures & Algorithms/anagram-groups/submission-8.py
@@ -0,0 +1,36 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        # result = [[strs[0]]]
+        # for i in range(1, len(strs)):
+        #     temp = True
+        #     # print(result)
+        #     for j in range(len(result)):
+        #         for word in result[j]:
+        #             # print(strs[i], word)
+        #             # print(len(strs[i]), len(word))
+        #             if len(strs[i]) == len(word) and Counter(strs[i]) == Counter(word):
+        #                 # print(Counter(strs[i]), Counter(word))
+        #                 result[j].append(strs[i])
+        #                 temp = False
+        #                 break
+        #         if not temp:
+        #             break
+        #     if temp:
+        #         result.append([strs[i]])
+        # return result
+
+        result = defaultdict(list)
+
+        for str in strs:
+            count = [0] * 26
+
+            for c in str:
+                count[ord(c) - ord("a")] += 1
+
+            result[tuple(count)].append(str)
+        
+        return list(result.values())
+
+
+
+

aaea7621c154669b39f2850b2fb3b1888b90156b
zfan747 <fanzhen0408@gmail.com>
2026-06-12 21:17:37 +1200

    Add: string-encode-and-decode - submission-9

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-9.py
@@ -0,0 +1,16 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) > 0:
+            result = strs[0]
+        else:
+            return "None"
+        for i in range(1, len(strs)):
+            print("buh")
+            result += "-"
+            result += strs[i]
+        return result
+    def decode(self, s: str) -> List[str]:
+        if s == "None":
+            return []
+        else:
+            return s.split("-")

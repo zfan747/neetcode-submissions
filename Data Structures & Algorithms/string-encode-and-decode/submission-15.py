d43dd3b0d36dacb2dcf81ca89afd6a1e01562124
zfan747 <fanzhen0408@gmail.com>
2026-06-12 21:23:11 +1200

    Add: string-encode-and-decode - submission-15

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-15.py
@@ -0,0 +1,15 @@
+class Solution:
+    def encode(self, strs: List[str]) -> str:
+        if len(strs) > 0:
+            result = strs[0]
+        else:
+            return "NULL"
+        for i in range(1, len(strs)):
+            result += "!#!"
+            result += strs[i]
+        return result
+    def decode(self, s: str) -> List[str]:
+        if s == "NULL":
+            return []
+        else:
+            return s.split("!#!")
